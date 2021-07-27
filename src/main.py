cf4e8f838d189e6ce09508588fba82aad400a439
namrenard <nam.renard@gmail.com>
2021-07-27 22:13:27 +0200

    V0.4.1 - fixed bug on reloading the program.

--- src/main.py
+++ src/main.py
@@ -34,46 +34,46 @@ from random import shuffle
 # definition class word_programme
 
 class Word:
-    def __init__(self, word):
-        self.word = word
 
-    #function shuffle for a word
+    def __init__(self, word, token=0):
+        self.word = word
+        self.token = token
+
+
+    # function shuffle for a word
     def anagramme(self):
         a = list(self.word)
         shuffle(a)
         return "".join(a)
 
     # choice of shuffle
-    def anagramme_multiple(self):
-
+    def anagramme_multiple(self,token):
         nombre_de_recherche = ''
         y = 1
         while nombre_de_recherche == '' or nombre_de_recherche.isdigit() == False:
-            nombre_de_recherche = input("Choisissez le nombre de recherche que vous souhaitez ?:")
-
+            nombre_de_recherche = input("Choisissez le nombre d'anagramme à générer:")
         nombre_de_recherche_int = int(nombre_de_recherche)
-        print(f"Vous recherchez {nombre_de_recherche} anagrammes de ({self.word})----")
+        print(f"Voici les résultats pour {nombre_de_recherche} anagrammes du mot ({self.word}):")
+        print()
         while y <= nombre_de_recherche_int:
             print("".join(self.anagramme()))
             y = y+1
-        self.relance_anagramme()
+        if token == 0:
+            self.relance_anagramme()
+        else:
+            exit()
 
     # again a shuffle of shutdown the program?
-    def relance_anagramme(self, ):
+    def relance_anagramme(self):
         question = input("Relancez le générateur avec le même mot ? o/n")
-        self.stop = stop
         if question.lower() == "o":
-            self.anagramme_multiple()
-
-
+            self.anagramme_multiple(1)
         elif question.lower() == "n":
-            print("Ok. Programme Terminé")
-
+             print("Ok. Programme Terminé")
         else:
             print("Vous n'avez pas donné de réponse valide. Programme Terminé")
-            self.relance_anagramme(False)
-        if self.stop == True:
-            exit()
+            self.relance_anagramme()
+
 
     # check if it's a word or number // miss to block the mixture of word+number
     def verification_anagramme(self):
@@ -99,5 +99,5 @@ while mot == '':
 
 mot_str = Word(mot)
 mot_str.verification_anagramme()
-mot_str.anagramme_multiple()
+mot_str.anagramme_multiple(0)
 # mot_str.relance_anagramme()
